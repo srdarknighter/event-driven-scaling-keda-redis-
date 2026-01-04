f369e013107cc497c65c19fabe27da49e317394f
srdarknighter <anirudhiyer05@gmail.com>
2026-01-04 11:43:50 +0530

    fix: issues fixed

--- judge-worker/go.mod
+++ judge-worker/go.mod
@@ -3,10 +3,13 @@ module judge-worker
 go 1.22.0
 
 require (
+	github.com/google/uuid v1.6.0
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.17.2
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/jmoiron/sqlx v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/redis/go-redis/v9 v9.17.2 // indirect
 )
--- judge-worker/load.js
+++ judge-worker/load.js
@@ -19,7 +19,8 @@ function randomString(length) {
 }
 
 export default function () {
-    let tier = Math.random() > (1 / 6) ? "free" : "premium";
+    let tier = Math.random() > 0.833 ? "premium" : "free";  // 1/6 = 0.166 premium
+    
     const payload = JSON.stringify({
         submission_id: randomString(8),
         user_id: 'user_' + randomString(5),
@@ -31,7 +32,7 @@ export default function () {
         headers: { 'Content-Type': 'application/json' }
     };
 
-    http.post('http://localhost:8080/submit', payload, params);
+    http.post('http://localhost:30080/submit', payload, params);  // Changed port to 30080
 
     sleep(0.1);
 }
